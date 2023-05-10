5344c945556e5b6429a80a22bb30205bf7642612
teetoz <teetoz>
2023-05-10 20:58:10 +0400

    fields cannot be empty

--- module00/ex01/PhoneBook.cpp
+++ module00/ex01/PhoneBook.cpp
@@ -28,18 +28,43 @@ void	PhoneBook::addContact(void)
 {
 	std::cout << "\nKindly input the information of the contact one field at a time: " << std::endl;
 	std::cout << std::endl;
-	std::cout << "\tFirst Name: ";
-	contacts[ContactNum].setFirstName();
-	std::cout << "\tLast Name: ";
-	contacts[ContactNum].setLastName();
-	std::cout << "\tNickname: ";
-	contacts[ContactNum].setNickname();
-	std::cout << "\tPhone number: ";
-	contacts[ContactNum].setPhoneNumber();
-	std::cout << "\tDarkest secret: ";
-	contacts[ContactNum].setDarkestSecret();
+	while (contacts[ContactNum].getFirstName().empty())
+	{
+		std::cout << "\tFirst Name: ";
+		contacts[ContactNum].setFirstName();
+		if (contacts[ContactNum].getFirstName().empty())
+			std::cout << "\t\tCannot be empty!\n";
+	}
+	while (contacts[ContactNum].getLastName().empty())
+	{
+		std::cout << "\tLast Name: ";
+		contacts[ContactNum].setLastName();
+		if (contacts[ContactNum].getLastName().empty())
+			std::cout << "\t\tCannot be empty!\n";
+	}
+	while (contacts[ContactNum].getNickname().empty())
+	{
+		std::cout << "\tNickname: ";
+		contacts[ContactNum].setNickname();
+		if (contacts[ContactNum].getNickname().empty())
+			std::cout << "\t\tCannot be empty!\n";
+	}
+	while (contacts[ContactNum].getPhoneNumber().empty())
+	{
+		std::cout << "\tPhone Number: ";
+		contacts[ContactNum].setPhoneNumber();
+		if (contacts[ContactNum].getPhoneNumber().empty())
+			std::cout << "\t\tCannot be empty!\n";
+	}
+	while (contacts[ContactNum].getDarkestSecret().empty())
+	{
+		std::cout << "\tDarkest Secret: ";
+		contacts[ContactNum].setDarkestSecret();
+		if (contacts[ContactNum].getDarkestSecret().empty())
+			std::cout << "\t\tCannot be empty!\n";
+	}
 	std::cout << std::endl;
-	std::cout << "Information of " << contacts[ContactNum].getFirstName() << " is now saved with index " << ContactNum + 1 << std::endl;
+	std::cout << "Information of " << contacts[ContactNum].getFirstName() << " is now saved under index " << ContactNum + 1 << std::endl;
 	std::cout << std::endl;
 	ContactNum++;
 	if (ContactNum == 8)
