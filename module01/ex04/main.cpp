52b48d6402313148a9f22fd59edb2cf3be2c2629
Talal Alsaiaari <talsaiaa@lab1r1s5.42abudhabi.ae>
2023-05-13 19:34:33 +0400

    ex04 works with replace func but not allowed

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,19 +6,42 @@
 /*   By: talsaiaa <talsaiaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 14:19:32 by talsaiaa          #+#    #+#             */
-/*   Updated: 2023/05/13 14:32:36 by talsaiaa         ###   ########.fr       */
+/*   Updated: 2023/05/13 19:29:48 by talsaiaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
+#include <fstream>
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	if (ac != 4)
+	std::string	line;
+	std::string	args[3];
+	std::size_t	pos;
+
+	if (ac != 4 || !av[2][0] )
 	{
-		std::cout << "Invalid number of arguments!" << std::endl;
-		return 1;
+	std::cout << "Invalid number of arguments!" << std::endl;
+	return 1;
+	}
+	av++;
+	for (int i = 0; i < ac - 1; i++)
+	args[i] = av[i];
+
+	std::ifstream infile(args[0]);
+	std::cin.rdbuf(infile.rdbuf()); // Redirect std::cin to errors_to_handle.txt
+
+	std::ofstream outfile(args[0] + ".replace");
+	std::cout.rdbuf(outfile.rdbuf()); // Redirect std::cout to outfile.txt
+
+	while (std::getline(std::cin, line))
+	{
+		pos = line.find(args[1]);
+		// if (pos != std::string::npos)
+		// {
+			
+		// }
+		std::cout << line.replace(pos, args[1].length(), args[2]) << std::endl; // This prints the contents of errors_to_handle.txt
 	}
 	return 0;
 }
