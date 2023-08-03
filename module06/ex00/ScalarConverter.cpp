217832ffa420f9351e89e6aceb9a125fbeecdce8
teetoz <teetoz>
2023-08-03 13:26:36 +0400

    normal numbers working

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: talsaiaa <talsaiaa@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 18:54:17 by talsaiaa          #+#    #+#             */
-/*   Updated: 2023/08/02 10:42:16 by talsaiaa         ###   ########.fr       */
+/*   Updated: 2023/08/03 13:26:05 by talsaiaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 ScalarConverter::ScalarConverter(void)
 {
-	nums = "1234567890";
+	;
 }
 
 bool ScalarConverter::isChar(std::string arg)
 {	
+	std::string nums = "1234567890";
 	if (arg.length() == 1 && arg.find_first_of(nums) == std::string::npos)
 		return true;
 	return false;
@@ -26,6 +27,7 @@ bool ScalarConverter::isChar(std::string arg)
 
 bool ScalarConverter::isInt(std::string arg)
 {	
+	std::string nums = "1234567890";
 	if (arg.find_first_not_of(nums) == std::string::npos)
 		return true;
 	if (arg[0] == '-' && arg.find_first_not_of(nums, 1) == std::string::npos)
@@ -38,6 +40,7 @@ bool ScalarConverter::isInt(std::string arg)
 bool ScalarConverter::isFloat(std::string arg)
 {
 	size_t dotPos = arg.find_first_of(".");
+	std::string nums = "1234567890";
 	
 	if (arg[arg.length()] == 'f'
 		&& arg.find_last_not_of(nums + ".", arg.length() - 1) == std::string::npos
@@ -49,9 +52,131 @@ bool ScalarConverter::isFloat(std::string arg)
 bool ScalarConverter::isDouble(std::string arg)
 {
 	size_t dotPos = arg.find_first_of(".");
+	std::string nums = "1234567890";
 
 	if (arg.find_first_not_of(nums + ".")
 		&& arg.find_last_of(".") == dotPos)
 		return true;
 	return false;
+}
+
+void ScalarConverter::convertToChar(std::string arg)
+{
+	std::stringstream conv;
+	char c;
+
+	conv << arg;
+	conv >> c;
+	charToAll(c);
+}
+
+void ScalarConverter::convertToInt(std::string arg)
+{
+	std::stringstream conv;
+	long n;
+
+	conv << arg;
+	conv >> n;
+	intToAll(n);
+}
+
+void ScalarConverter::convertToFloat(std::string arg)
+{
+	std::stringstream conv;
+	float f;
+
+	conv << arg;
+	conv >> f;
+	floatToAll(f);
+}
+
+void ScalarConverter::convertToDouble(std::string arg)
+{
+	std::stringstream conv;
+	double d;
+
+	conv << arg;
+	conv >> d;
+	doubleToAll(d);
+}
+
+void ScalarConverter::charToAll(char c)
+{
+	displayChar(c);
+	displayInt(static_cast <int> (c));
+	displayFloat(static_cast <float> (c));
+	displayDouble(static_cast <double> (c));
+}
+
+void ScalarConverter::intToAll(long n)
+{
+	displayChar(static_cast <char> (n));
+	displayInt(n);
+	displayFloat(static_cast <float> (n));
+	displayDouble(static_cast <double> (n));
+}
+
+void ScalarConverter::floatToAll(float f)
+{
+	displayChar(static_cast <char> (f));
+	displayInt(static_cast <int> (f));
+	displayFloat((f));
+	displayDouble(static_cast <double> (f));
+}
+
+void ScalarConverter::doubleToAll(double d)
+{
+	displayChar(static_cast <char> (d));
+	displayInt(static_cast <int> (d));
+	displayFloat(static_cast <float> (d));
+	displayDouble(d);
+}
+
+void ScalarConverter::displayChar(char c)
+{
+	if (c >= 33 && c <= 126)
+		std::cout << "char: '" << c  << "'" << std::endl;
+	else if ((c >= 0 && c < 33) || c == 127)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: impossible" << std::endl;
+}
+
+void ScalarConverter::displayInt(long n)
+{
+	if (n < INT_MIN || n > INT_MAX)
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << static_cast <int> (n) << std::endl;
+}
+
+void ScalarConverter::displayFloat(float f)
+{
+	// std::cout << FLT_MIN << std::endl;
+	// std::cout << f << std::endl;
+	// std::cout << typeid(f).name() << std::endl;
+	// if (f < FLT_MIN || f > FLT_MAX)
+	// 	std::cout << "float: impossible" << std::endl;
+	// else
+	std::cout << "float: " << f << ".0f" << std::endl;
+}
+
+void ScalarConverter::displayDouble(double d)
+{
+	// if (d < DBL_MIN || d > DBL_MAX)
+	// 	std::cout << "double: impossible" << std::endl;
+	// else
+	std::cout << "double: " << d << ".0" << std::endl;
+}
+
+void ScalarConverter::convert(std::string arg)
+{
+	if (isChar(arg))
+		convertToChar(arg);
+	else if (isInt(arg))
+		convertToInt(arg);
+	else if (isFloat(arg))
+		convertToFloat(arg);
+	else if (isDouble(arg))
+		convertToDouble(arg);
 }
--- module06/ex00/ScalarConverter.hpp
+++ module06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: talsaiaa <talsaiaa@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 18:07:29 by talsaiaa          #+#    #+#             */
-/*   Updated: 2023/08/02 10:47:04 by talsaiaa         ###   ########.fr       */
+/*   Updated: 2023/08/03 13:21:42 by talsaiaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,30 @@
 
 #include <iostream>
 #include <sstream>
-#include <limits>
+#include <climits>
+#include <cfloat>
+#include <typeinfo>
 
 class ScalarConverter
 {
 	private:
-		static std::string nums;
-		static char c;
-		static long int n;
-		static float f;
-		static double d;
-		
 		ScalarConverter();
 		static bool isChar(std::string);
 		static bool isInt(std::string);
 		static bool isFloat(std::string);
 		static bool isDouble(std::string);
-		static void convertToChar(std::stringstream);
-		static void convertToInt(std::stringstream);
-		static void convertToFloat(std::stringstream);
-		static void convertToDouble(std::stringstream);
-		static void detectType(std::stringstream);
+		static void convertToChar(std::string);
+		static void convertToInt(std::string);
+		static void convertToFloat(std::string);
+		static void convertToDouble(std::string);
+		static void charToAll(char);
+		static void intToAll(long);
+		static void floatToAll(float);
+		static void doubleToAll(double);
+		static void displayChar(char);
+		static void displayInt(long);
+		static void displayFloat(float);
+		static void displayDouble(double);
 
 	public:
 		static void convert(std::string);
