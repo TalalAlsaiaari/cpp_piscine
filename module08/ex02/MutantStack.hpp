63407279641461622c0744a4e7757b3f94514cbf
teetoz <teetoz>
2023-08-09 20:42:03 +0400

    starting moudle08 ex02

---
+++ module08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: talsaiaa <talsaiaa@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 20:10:30 by talsaiaa          #+#    #+#             */
+/*   Updated: 2023/08/09 20:15:46 by talsaiaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <stack>
+
+template <typename T> class MutanStack : public std::stack
+{
+	
+};
+
+#include "MutantStack.tpp"
